4ea7c0782b9ff3eb18776419128510f257c48e28
Holly1015 <hollyman15si@yahoo.co.jp>
2025-03-16 17:19:32 +0900

    初期セットアップ

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,14 @@
 import { type RouteConfig, index, prefix, layout, route } from "@react-router/dev/routes";
 
 export default [
+
+    // route("products", "./page/admin/product/Products.tsx"),
+    route("productsInsert", "./page/admin/product/ProductsInsert.tsx"),
+    
+    // ...prefix("products", [
+    //     route(":productId", "./page/admin/product/ProductsDetail.tsx"),
+    //     ]),
+
     route("admin", "./layout/admin/admin-layout.tsx", [
         index("./page/admin/product/search-product.tsx"),
        // route("search-product", "./page/admin/product/search-product.tsx")
