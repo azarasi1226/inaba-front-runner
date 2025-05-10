3a1b9427e68cca9657c8658707c8fa50d5fff224
andoukazuki <seal1226.jp@gmail.com>
2025-05-10 18:25:18 +0900

    save

--- app/page/admin/product/search/loader.tsx
+++ app/page/admin/product/search/loader.tsx
@@ -5,8 +5,7 @@ import { getDefaultQuerys } from "./get-default-querys";
 const PAGE_SIZE = 10;
 
 export async function loader({ request }: Route.LoaderArgs) {
-  const url = new URL(request.url);
-  const defaultQuerys = getDefaultQuerys(url.searchParams);
+  const defaultQuerys = getDefaultQuerys(new URL(request.url).searchParams);
 
   const { data, error } = await openApiFetchClient.GET("/api/products", {
     params: {
